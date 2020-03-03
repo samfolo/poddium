9a74811e37884bc80c5fa0197866d6d181767350
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-03 20:28:59 +0000

    <LoginPage /> initially renders `Sign up` and `Sign in` buttons

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -3,7 +3,10 @@ import Classes from './LoginPage.module.css';
 
 class LoginPage extends React.Component {
   render() {
-    return <div className={Classes.LoginPage} data-test="component-login-page" />
+    return <div className={Classes.LoginPage} data-test="component-login-page">
+      <div data-test="sign-up">Sign up</div>
+      <div data-test="sign-in">Sign in</div>
+    </div>
   }
 }
 
--- src/containers/LoginPage/LoginPage.test.js
+++ src/containers/LoginPage/LoginPage.test.js
@@ -13,4 +13,16 @@ describe('<LoginPage />', () => {
   it('renders without error', () => {
     expect(loginPageContainer).toHaveLength(1);
   });
+
+  it('initially renders a `Sign up` button', () => {
+    const signUpButton = findByTestAttr(wrapper, 'sign-up');
+    expect(signUpButton).toHaveLength(1);
+    expect(signUpButton.text()).toEqual('Sign up');
+  });
+
+  it('initially renders a `Sign in` button', () => {
+    const signInButton = findByTestAttr(wrapper, 'sign-in');
+    expect(signInButton).toHaveLength(1);
+    expect(signInButton.text()).toEqual('Sign in');
+  });
 })
