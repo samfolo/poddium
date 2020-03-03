02baca69f144e56793daf3299d006ebbf7a7382b
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-03 20:53:18 +0000

    feat: <LoginPage /> renders a `Sign up` form when user wants to sign up

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -4,15 +4,21 @@ import Classes from './LoginPage.module.css';
 class LoginPage extends React.Component {
   state = {
     isSignIn: false,
+    isSignUp: false,
   }
 
   render() {
     const signInForm = this.state.isSignIn ? <div data-test="sign-in-form" /> : null;
+    const signUpForm = this.state.isSignUp ? <div data-test="sign-up-form" /> : null;
+    const buttons = this.state.isSignIn || this.state.isSignUp ? null : [
+      <div key="sign-up" data-test="sign-up">Sign up</div>,
+      <div key="sign-in" data-test="sign-in">Sign in</div>,
+    ]
 
     return <div className={Classes.LoginPage} data-test="component-login-page">
       {signInForm}
-      <div data-test="sign-up">Sign up</div>
-      <div data-test="sign-in">Sign in</div>
+      {signUpForm}
+      {buttons}
     </div>
   }
 }
--- src/containers/LoginPage/LoginPage.test.js
+++ src/containers/LoginPage/LoginPage.test.js
@@ -4,6 +4,8 @@ import { setup, findByTestAttr } from '../../testHelpers';
 describe('<LoginPage />', () => {
   let wrapper;
   let loginPageContainer;
+  let signInButton;
+  let signUpButton;
 
   beforeEach(() => {
     wrapper = setup(LoginPage);
@@ -15,13 +17,13 @@ describe('<LoginPage />', () => {
   });
 
   it('initially renders a `Sign up` button', () => {
-    const signUpButton = findByTestAttr(wrapper, 'sign-up');
+    signUpButton = findByTestAttr(wrapper, 'sign-up');
     expect(signUpButton).toHaveLength(1);
     expect(signUpButton.text()).toEqual('Sign up');
   });
 
   it('initially renders a `Sign in` button', () => {
-    const signInButton = findByTestAttr(wrapper, 'sign-in');
+    signInButton = findByTestAttr(wrapper, 'sign-in');
     expect(signInButton).toHaveLength(1);
     expect(signInButton.text()).toEqual('Sign in');
   });
@@ -35,11 +37,13 @@ describe('<LoginPage />', () => {
       signUpForm = findByTestAttr(wrapper, 'sign-up-form');
     }
 
-    beforeEach(() => {
-      findAllForms();
-    });
+    const findAllButtons = () => {
+      signInButton = findByTestAttr(wrapper, 'sign-in');
+      signUpButton = findByTestAttr(wrapper, 'sign-up');
+    }
 
     it('does not initially render any form', () => {
+      findAllForms();
       expect(signInForm).toHaveLength(0);
       expect(signUpForm).toHaveLength(0);
     });
@@ -47,9 +51,22 @@ describe('<LoginPage />', () => {
     it('renders a `Sign in` form when user wants to sign in', () => {
       wrapper = setup(LoginPage, {}, { isSignIn: true });
       findAllForms();
+      findAllButtons();
 
       expect(signInForm).toHaveLength(1);
       expect(signUpForm).toHaveLength(0);
+      expect(signInButton).toHaveLength(0);
+      expect(signUpButton).toHaveLength(0);
+    });
+
+    it('renders a `Sign up` form when user wants so sign up', () => {
+      wrapper = setup(LoginPage, {}, { isSignUp: true });
+      findAllForms();
+
+      expect(signInForm).toHaveLength(0);
+      expect(signUpForm).toHaveLength(1);
+      expect(signInButton).toHaveLength(0);
+      expect(signUpButton).toHaveLength(0);
     });
   })
 })
