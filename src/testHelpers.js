cbd188a62399b731eda8fa6299cd08baa11c5a6d
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-03 19:23:20 +0000

    setup: testHelpers.js and components folder

---
+++ src/testHelpers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+export const setup = (Component, props = {}, state = null) => {
+  let wrapper = shallow(<Component { ...props } />);
+  if (state) wrapper.setState({ state });
+  return wrapper;
+}
+
+export const findByTestAttr = (wrapper, testAttr) => wrapper.find(`[data-test='${testAttr}']`);
