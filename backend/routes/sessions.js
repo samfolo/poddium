525cf126b78cb5867d55cc2c3383efaaa31b5d6b
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-05 22:23:16 +0000

    feat: create /sessions/new route for login

---
+++ backend/routes/sessions.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+  return res.json('nothing here yet');
+});
+
+router.route('/new').post((req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.find({ email: email })
+  .then(users => {
+    const foundUser = users[0];
+    if (foundUser.password === password) {
+      return res.json('authorised');
+    } else {
+      throw new Error('Invalid Password')
+    }
+  })
+  .catch(error => res.status(400).json(`${error}`));
+});
+
+module.exports = router;
