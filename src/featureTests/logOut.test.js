29bdbe06fc1dee6412a8ed4267d090e2ce16fab1
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-05 11:20:49 +0000

    pause: logoOut.test.js async test

--- src/featureTests/logOut.test.js
+++ src/featureTests/logOut.test.js
@@ -1,7 +1,7 @@
 import { mountedSetup, findByTestAttr, signUp, expectLengthOf } from '../testHelpers';
 import App from '../containers/App/App';
 
-describe('logging out', () => {
+xdescribe('logging out', () => {
   let wrapper;
   
   beforeEach(() => {
@@ -9,21 +9,21 @@ describe('logging out', () => {
   });
 
   describe('a valid signup and logout', () => {
-    it('1', () => expect(1).toBe(1))
-    // test('a user named Saul Goodman signs up then logs out', () => {
-    //   const saulGoodman = {
-    //     username: 'Saul2 Goodman',
-    //     email: 'saul2@example.com',
-    //     password: '1234icecream',
-    //     passwordConfirmation: '1234icecream'
-    //   }
-        
-    //   signUp(wrapper, saulGoodman);
-    //   const logOutButton = findByTestAttr(wrapper, 'log-out');
-    //   logOutButton.simulate('click');
+    test('a user named Saul Goodman signs up then logs out', () => {
+      const saulGoodman = {
+        username: 'Saul Goodman',
+        email: 'saul@example.com',
+        password: '1234icecream',
+        passwordConfirmation: '1234icecream'
+      }
 
-    //   const logInPage = findByTestAttr(wrapper, 'component-login-page');
-    //   expect(logInPage).toHaveLength(1);
-    // });
+      signUp(wrapper, saulGoodman)
+
+      const logOutButton = findByTestAttr(wrapper, 'log-out');
+      logOutButton.simulate('click');
+
+      const logInPage = findByTestAttr(wrapper, 'component-login-page');
+      expect(logInPage).toHaveLength(1);
+    });
   });
 });
