b4bf9e3c33b9eb1f1cddbd6d4971b9b89b42104e
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-06 15:29:12 +0000

    refactor: <ShowList /> component will render the shows

--- src/components/ShowList/ShowList.js
+++ src/components/ShowList/ShowList.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 class ShowList extends React.Component {
+  renderShows = () => {
+    const shows = this.props.shows;
+    const showComponents = shows.map(show => {
+      return <div 
+        key={`${show.id}`}
+        data-test="show" />;
+    });
+
+    return showComponents;
+  }
+
   render() {
     return (
-      <div data-test="component-show-list" />
+      <div data-test="component-show-list">
+        {this.renderShows()}
+      </div>
     )
   }
 }
--- src/components/ShowList/ShowList.test.js
+++ src/components/ShowList/ShowList.test.js
@@ -1,17 +1,37 @@
 import ShowList from './ShowList';
-import { setup, findByTestAttr } from "../../testHelpers";
+import { setup, findByTestAttr, expectLengthOf } from "../../testHelpers";
+
+import mockResponses from '../../util/mockApiResonses';
 
 describe('<ShowList />', () => {
   let wrapper;
-  let showListComponent;
+  let showListComponent
+  let shows;
+
+  const getShows = (number = 20) => {
+    return mockResponses.getGenericPodcasts().slice(0, number);
+  }
 
   beforeEach(() => {
-    wrapper = setup(ShowList);
+    wrapper = setup(ShowList,{ shows: getShows(5) });
     showListComponent = findByTestAttr(wrapper, 'component-show-list');
   })
   it('renders without error', () => {
     expect(showListComponent).toHaveLength(1);
   });
 
-  it('renders a show when passed a show', () => {})
+  describe('rendering shows', () => {
+    const runTestWithShows = number => {
+      shows = getShows(number)
+      wrapper = setup(ShowList, { shows });
+      expectLengthOf(wrapper, 'show').toBe(number);
+    }
+
+    it('renders a show when passed a single show', () => runTestWithShows(1));
+    it('renders ten shows when passed ten shows', () => runTestWithShows(10));
+    it('renders fourteen shows when passed fourteen shows', () => runTestWithShows(14));
+    it('renders six shows when passed six shows', () => runTestWithShows(6));
+  
+  });
+
 })
