bb6eb482167e4b0be3323ef84c6f7cb3efba979d
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-06 16:25:07 +0000

    feat: present spotify api data through <Show /> components

--- src/components/ShowList/ShowList.js
+++ src/components/ShowList/ShowList.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Show from '../Show/Show';
 
 class ShowList extends React.Component {
   renderShows = () => {
     const shows = this.props.shows;
     const showComponents = shows.map(show => {
-      return <div 
+      return <Show 
         key={`${show.id}`}
-        data-test="show" />;
+        data-test="show"
+        name={show.name}
+        image={show.image}
+        description={show.description}
+        publisher={show.publisher} />;
     });
 
     return showComponents;
