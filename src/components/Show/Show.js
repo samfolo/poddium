f0cafb3b1aaeebfe4f5f2b31aeb3f67ea2c608d3
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-06 16:20:10 +0000

    feat: <Show /> renders name, description, image and publisher

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -3,7 +3,12 @@ import Classes from './Show.module.css';
 
 const Show = props => {
   return (
-    <div className={Classes.Show} data-test="component-show" />
+    <div className={Classes.Show} data-test="component-show">
+      <h1 data-test="name">{props.name}</h1>
+      <img data-test="image" src={props.image.url} alt={props.name} />
+      <div data-test="description">{props.description}</div>
+      <div data-test="publisher">{props.publisher}</div>
+    </div>
   )
 }
 
