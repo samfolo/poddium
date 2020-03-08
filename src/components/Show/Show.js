9d89f1a52b8bd4cfffe1bd6f64d29fb51aac05a8
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-08 20:19:52 +0000

    style: add show link on hover

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Classes from './Show.module.css';
 
 const Show = props => {
+  const [showsIcon, setShowsIcon] = useState(false);
+  
+  const showIcon = () => {
+    setShowsIcon(true);
+  }
+
+  const hideIcon = () => {
+    setShowsIcon(false);
+  }
+
   return (
     <div className={Classes.Show} data-test="component-show">
       <div className={Classes.Content}>
         <h1 data-test="name" className={Classes.Name}>{props.name}</h1>
         <div data-test="publisher" className={Classes.Publisher}>{props.publisher}</div>
-        <img data-test="image" className={Classes.Image} src={props.image.url} alt={props.name} />
+        <div 
+          data-test="image" 
+          onMouseEnter={showIcon}
+          onMouseLeave={hideIcon}
+          className={Classes.ImageContainer}
+          style={{ background: `url(${props.image.url}) no-repeat center center`, backgroundSize: 'cover'  }}>
+            <img 
+              className={Classes.Icon}
+              src="../../podcast-microphone-overlay.png"
+              style={{ opacity: showsIcon ? '1' : '0' }} onMouseOver={showIcon} />
+        </div>
         <div data-test="description" className={Classes.Description}>{props.description}</div>
       </div>
     </div>
