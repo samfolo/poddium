7253a82a17f888a2dca57c99dcb8872a8b18e80f
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-09 11:18:33 +0000

    feat: <EpisodeList /> renders episodes from props

--- src/components/EpisodeList/EpisodeList.js
+++ src/components/EpisodeList/EpisodeList.js
@@ -2,9 +2,23 @@ import React from 'react';
 import Classes from './EpisodeList.module.css';
 
 class EpisodeList extends React.Component {
+  renderEpisodes = () => {
+    const episodes = this.props.episodes;
+    const episodeComponents = episodes.map((episode, i) => {
+      return (
+        <div
+          key={episode.id}
+          data-test="episode" />
+      );
+    });
+    return episodeComponents;
+  }
+
   render() {
     return (
-      <div data-test="component-episode-list" />
+      <div data-test="component-episode-list">
+        {this.renderEpisodes()}
+      </div>
     );
   }
 }
