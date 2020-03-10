22928b7fdc2615757ad9d39951ddd1ab2b004d48
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-10 17:35:29 +0000

    feat: <NavButton /> renders a <NavLink /> from `react-router-dom`

--- src/NavButton/NavButton.js
+++ src/NavButton/NavButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NavButton = props => {
   return (
     <li data-test="component-nav-button">
-
+      <NavLink to={props.link} exact data-test="navlink" />
     </li>
   )
 }
