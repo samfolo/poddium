b79d157ffb439e2b5b2cf2d6cad58696573d43ed
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-16 21:03:41 +0000

    feat: <AudioPlayer /> can play a track from props

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import Classes from './AudioPlayer.module.css';
+import { register } from '../../serviceWorker';
 
 class AudioPlayer extends React.Component {
+  state = {
+    hasEpisodeLoaded: false,
+    isPlaying: false,
+  }
+
+  componentDidMount() {
+    if (this.props.episode) {
+      this.setState({ hasEpisodeLoaded: true });
+    } else {
+      this.setState({ hasEpisodeLoaded: false });
+    }
+  }
+
+  play = () => {
+    let audio = new Audio();
+    audio.src = this.props.episode.url;
+    audio.play();
+    this.setState({ isPlaying: true });
+  }
+
   render() {
+    let playbackControls = <div data-test="play-button" onClick={this.play}></div>;
+    let episodeTitle = null;
+
+    if (this.state.isPlaying) {
+      playbackControls = <div data-test="pause-button"></div>;
+      episodeTitle = <div data-test="episode-name">{this.props.episode.name}</div>
+    }
+
     return (
       <div className={Classes.AudioPlayer} data-test="component-audio-player">
-        <div data-test="play-button"></div>
+<       div className={Classes.Button}>
+          {playbackControls}
+          {episodeTitle}
+        </div>
       </div>
     );
   }
--- src/components/AudioPlayer/AudioPlayer.test.js
+++ src/components/AudioPlayer/AudioPlayer.test.js
@@ -4,12 +4,36 @@ import { setup, findByTestAttr, expectLengthOf } from '../../testHelpers';
 describe('<AudioPlayer />', () => {
   let wrapper;
   let audioPlayerComponent;
+  let defaultProps = {
+    episode: {
+      name: 'Test episode', 
+      url: 'https://test.example/test-episode',
+    }
+  };
+
+  let defaultState = {
+    hasEpisodeLoaded: true,
+    isPlaying: false,
+  }
 
   beforeEach(() => {
     wrapper = setup(AudioPlayer);
     audioPlayerComponent = findByTestAttr(wrapper, 'component-audio-player');
+    window.HTMLMediaElement.prototype.load = jest.fn();
+    window.HTMLMediaElement.prototype.play = jest.fn();
+    window.HTMLMediaElement.prototype.pause = jest.fn();
   });
 
   it('renders without error', () => expect(audioPlayerComponent).toHaveLength(1));
-  it('renders a play button', () => expectLengthOf(wrapper, 'play-button').toBe(1));
+  it('initially renders a play button', () => expectLengthOf(wrapper, 'play-button').toBe(1));
+
+  describe('when a track is loaded', () => {
+    it('can play a track from props', () => {
+      wrapper = setup(AudioPlayer, defaultProps, defaultState);
+      const playButton = findByTestAttr(wrapper, 'play-button');
+      playButton.simulate('click');
+      expect(wrapper.text()).toContain('Test episode');
+      expectLengthOf(wrapper, 'pause-button').toBe(1);
+    });
+  });
 });
